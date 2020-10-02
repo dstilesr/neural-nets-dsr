c066fa626f4f0dfb7c95c729a4fa7fe346bfa8d9
dstilesr <d.stiles.r@hotmail.com>
2020-10-01 19:04:52 -0500

    Added ADAM optimizer v1

--- neural_nets_dsr/optim/__init__.py
+++ neural_nets_dsr/optim/__init__.py
@@ -1,4 +1,5 @@
 from .base import Optimizer
+from .adam import AdamOptimizer
 from .gradient_descent import GradientDescent
 from .minibatch_gradient_descent import MiniBatchGDL2
 from .regularized_gradient_descent import GradientDescentL2
